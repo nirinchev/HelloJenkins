699d1e75d64b94ae270dff054c65e4ab5b899a49
Nikola Irinchev <irinchev@me.com>
2015-06-09 14:23:17 +0300

    Initial commit

---
+++ HelloJenkins/Controllers/HelloController.cs
@@ -0,0 +1,19 @@
+﻿using System.Web.Http;
+
+namespace HelloJenkins.Controllers
+{
+	[RoutePrefix("api/hello")]
+    public class HelloController : ApiController
+    {
+		[Route("{name}")]
+		public IHttpActionResult Post(string name)
+		{
+			var result = new
+			{
+				Message = string.Format("Hello, {0}!", name)
+			};
+
+			return this.Ok(result);
+		}
+    }
+}
